2cff46e52ddffd45ad17afade7a0e5c0907f6e2e
Miles Porter <mporter@paintedharmony.com>
2017-11-01 14:09:09 -0500

    Adding script shortcut.

---
+++ myhs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Using:"
+echo $1
+echo $2
+echo $3
+echo $4
+hadoop fs -rm -r -f $4
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files $1,$2 -mapper "$1" -reducer "$2" -input $3 -output $4
