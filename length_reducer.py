cd9f9d4b5c077ea56b605aec1e0c4a0c13a13019
Miles Porter <mporter@paintedharmony.com>
2017-11-01 14:07:13 -0500

    Adding check for bad fields.

--- length_reducer.py
+++ length_reducer.py
@@ -18,16 +18,17 @@ class LengthReducer():
 
         for line in self.sysin:
             fields = line.split()
-            count = int(fields[0])
-            word = fields[1].lower()
-            if word != current_word:
-                self.save_data(current_word, word_count)
-                if key is not None and key == current_word:
-                    search_count = word_count
-                current_word = word
-                word_count = count
-            else:
-                word_count = word_count + count
+            if (len(fields)==2):
+                count = int(fields[0])
+                word = fields[1].lower()
+                if word != current_word:
+                    self.save_data(current_word, word_count)
+                    if key is not None and key == current_word:
+                        search_count = word_count
+                    current_word = word
+                    word_count = count
+                else:
+                    word_count = word_count + count
 
         # Don't forget the last page.
         self.save_data(word, word_count)
