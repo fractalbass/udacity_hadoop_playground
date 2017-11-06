501a1c3932ce73467cb0d4c344a85699aa9e2c00
Miles Porter <mporter@paintedharmony.com>
2017-11-06 14:01:16 -0600

    Not gonna work.

---
+++ mycomb.sh
@@ -0,0 +1,13 @@
+
+
+
+
+
+#!/bin/bash
+echo "Using:"
+echo $1
+echo $2
+echo $3
+echo $4
+hadoop fs -rm -r -f $4
+hadoop jar /usr/lib/hadoop-mapreduce/hadoop-streaming.jar -files $1,$2 -mapper "$1" -reducer "$2" -combiner $2 -input $3 -output $4
